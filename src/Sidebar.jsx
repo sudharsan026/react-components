0b153bfd64d8dc5cb5b77819acdaa6ff6743b15a
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-10-30 23:39:21 +0530

    Side bar items switches state using index

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState(null);
+  const switchActiveItem = (index) => {
+    setActiveItem(index === activeItem ? null : index);
+  };
   return (
     <ul className="list-wrapper">
-      <li className="active-list-item">Property settings</li>
-      <li className="default-list-item">Report</li>
-      <li className="default-list-item">Dashboard</li>
-      <li className="default-list-item">Questions</li>
-      <li className="default-list-item">Notifications</li>
+      <li
+        className={activeItem === 0 ? "active-list-item" : "default-list-item"}
+        onClick={() => switchActiveItem(0)}
+      >
+        Property settings
+      </li>
+      <li
+        className={activeItem === 1 ? "active-list-item" : "default-list-item"}
+        onClick={() => switchActiveItem(1)}
+      >
+        Report
+      </li>
+      <li
+        className={activeItem === 2 ? "active-list-item" : "default-list-item"}
+        onClick={() => switchActiveItem(2)}
+      >
+        Dashboard
+      </li>
+      <li
+        className={activeItem === 3 ? "active-list-item" : "default-list-item"}
+        onClick={() => switchActiveItem(3)}
+      >
+        Questions
+      </li>
+      <li
+        className={activeItem === 4 ? "active-list-item" : "default-list-item"}
+        onClick={() => switchActiveItem(4)}
+      >
+        Notifications
+      </li>
     </ul>
   );
 };
