e0eb8c6b120059b16df7def9b1da80174e190499
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-11-06 23:46:53 +0530

    Creating a table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import React from "react";
-import AutoComplete from "./AutoComplete.jsx";
-
-const suggestions = [
-  "Apple",
-  "Banana",
-  "Cherry",
-  "Date",
-  "Grapes",
-  "Lemon",
-  "Mango",
-  "Orange",
-  "Peach",
-  "Pineapple",
-];
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Autocomplete</h1>
-      <AutoComplete suggestions={suggestions} />
-    </div>
-  );
-}
+import React, { useMemo } from "react";
+import fakeData from "./MockData.json";
+import { useTable } from "react-table";
+const App = () => {
+  const data = useMemo(() => fakeData, []);
+  const columns = useMemo(() => [
+    { Header: "ID", accessor: "id" }, // accessor is the "key" in the data
+    { Header: "First name", accessor: "first_name" },
+    { Header: "Last name", accessor: "last_name" },
+    { Header: "Email", accessor: "email" },
+    { Header: "Gender", accessor: "gender" },
+    { Header: "University", accessor: "university" },
+  ]); //format of arrays
+  return <div>App</div>;
+};
 
 export default App;
