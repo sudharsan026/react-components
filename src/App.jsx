7f83bfbedf5aa9c7870397f88ef826797cdd1d4d
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-11-11 23:47:30 +0530

    toast and upload image component fucntionality template

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,65 @@
 import React, { useState } from "react";
-import countries from "./countries";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const App = () => {
-  const [country, setCountry] = useState(-1);
-  const handleCountryChange = (e) => {
-    let countrySelected = e.target.value;
-    setCountry(countrySelected);
-    console.log(countrySelected);
+const ImageUploader = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
   };
+
+  const uploadImage = () => {
+    if (!selectedFile) {
+      toast.error("Please select an image");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    axios
+      .post("/upload-endpoint", formData, {
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setUploadProgress(percentCompleted);
+        },
+      })
+      .then(() => {
+        toast.success("Image uploaded successfully");
+        // Additional logic after successful upload
+      })
+      .catch((error) => {
+        toast.error("Error uploading image");
+        console.error(error);
+      });
+  };
+
   return (
-    <div style={{ padding: "20px" }}>
-      <select className="form-select" onChange={handleCountryChange}>
-        {countries.map((_country, index) => (
-          <option key={_country.name} value={index}>
-            {_country.name}
-          </option>
-        ))}
-      </select>
-      <select className="form-select" style={{ marginTop: "24px" }}>
-        {countries[country]?.cities.map((_country) => (
-          <option>{_country}</option>
-        ))}
-      </select>
+    <div>
+      <input type="file" onChange={handleFileChange} accept="image/*" />
+      <button onClick={uploadImage}>Upload Image</button>
+
+      {uploadProgress > 0 && (
+        <div>
+          <p>Upload Progress: {uploadProgress}%</p>
+          <div
+            style={{
+              width: `${uploadProgress}%`,
+              height: "10px",
+              background: "#00b09b",
+            }}
+          ></div>
+        </div>
+      )}
+
+      <ToastContainer />
     </div>
   );
 };
 
-export default App;
+export default ImageUploader;
