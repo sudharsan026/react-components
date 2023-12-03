adfd767eda5d0a16907da8012f313eb1a9a92fcb
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-12-03 23:36:55 +0530

    Data fetching and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React from "react";
-import Cursor from "./components/cursor/cursor";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (response.data) {
+          setUsers(response.data);
+          setError("");
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(users);
+
   return (
-    <div style={{ backgroundColor: "black" }}>
-      <Cursor />
-    </div>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((_user) => (
+          <li key={_user.id}>{_user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
