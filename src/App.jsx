8606d90b1b9daf20900e0db63bb11207224ec668
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-12-05 23:58:53 +0530

    Simple loading bar with static control

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import React from "react";
-import StarRating from "./components/star-rating/star-rating";
+import React, { useState } from "react";
+import LoadingBar from "react-top-loading-bar";
 
 const App = () => {
+  const [progress, setProgress] = useState(0);
+
   return (
     <div>
-      <StarRating />
+      <LoadingBar
+        color="#f11946"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <div
+        style={{
+          margin: "20px",
+          display: "flex",
+          gap: "12px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <button
+          className="btn btn-primary"
+          onClick={() => setProgress(progress + 10)}
+        >
+          Add 10%
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => setProgress(progress + 20)}
+        >
+          Add 20%
+        </button>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setProgress(100)}
+        >
+          Complete
+        </button>
+      </div>
+      <br />
     </div>
   );
 };
