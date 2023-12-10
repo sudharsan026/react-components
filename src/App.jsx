eb7c76139ccb95e7bcb7606ddbb06d3e20ca6fa3
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-12-10 23:42:47 +0530

    Todos with drag n drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,112 @@
 import React, { useState } from "react";
-import LoadingBar from "react-top-loading-bar";
+import { v4 as uuidv4 } from "uuid";
+import { BsFillTrashFill, BsPencil, BsCheck, BsPlus } from "react-icons/bs";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
 
 const App = () => {
-  const [progress, setProgress] = useState(0);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [editableTaskId, setEditableTaskId] = useState(null);
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, { id: uuidv4(), text: newTask, completed: false }]);
+      setNewTask("");
+    }
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const editTask = (taskId, newText) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, text: newText } : task
+      )
+    );
+    setEditableTaskId(null); // After editing, set the task to non-editable
+  };
+
+  const toggleTask = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return; // Dropped outside the list
+    const reorderedTasks = Array.from(tasks);
+    const [movedTask] = reorderedTasks.splice(result.source.index, 1);
+    reorderedTasks.splice(result.destination.index, 0, movedTask);
+    setTasks(reorderedTasks);
+  };
 
   return (
-    <div>
-      <LoadingBar
-        color="#f11946"
-        progress={progress}
-        onLoaderFinished={() => setProgress(0)}
-      />
-      <div
-        style={{
-          margin: "20px",
-          display: "flex",
-          gap: "12px",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <button
-          className="btn btn-primary"
-          onClick={() => setProgress(progress + 10)}
-        >
-          Add 10%
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => setProgress(progress + 20)}
-        >
-          Add 20%
-        </button>
-        <button
-          className="btn btn-outline-primary"
-          onClick={() => setProgress(100)}
-        >
-          Complete
+    <div className="App">
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>
+          <BsPlus /> Add
         </button>
       </div>
-      <br />
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="tasks">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(provided) => (
+                    <li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <input
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => toggleTask(task.id)}
+                      />
+                      {editableTaskId === task.id ? (
+                        <>
+                          <input
+                            type="text"
+                            value={task.text}
+                            onChange={(e) => editTask(task.id, e.target.value)}
+                          />
+                          <button onClick={() => setEditableTaskId(null)}>
+                            <BsCheck /> Save
+                          </button>
+                        </>
+                      ) : (
+                        <>
+                          <span>{task.text}</span>
+                          <button onClick={() => setEditableTaskId(task.id)}>
+                            <BsPencil /> Edit
+                          </button>
+                        </>
+                      )}
+                      <button onClick={() => removeTask(task.id)}>
+                        <BsFillTrashFill /> Remove
+                      </button>
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
