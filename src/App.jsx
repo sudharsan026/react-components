5c64cefd196239959e7af69852100ef1aa73b6e8
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-12-21 23:59:25 +0530

    Users list first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,24 @@
 import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { BsFillTrashFill, BsPencil, BsCheck, BsPlus } from "react-icons/bs";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import "./App.css";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersLIst";
 
-const App = () => {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
-  const [editableTaskId, setEditableTaskId] = useState(null);
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  const addTask = () => {
-    if (newTask.trim() !== "") {
-      setTasks([...tasks, { id: uuidv4(), text: newTask, completed: false }]);
-      setNewTask("");
-    }
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
   };
-
-  const removeTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  };
-
-  const editTask = (taskId, newText) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === taskId ? { ...task, text: newText } : task
-      )
-    );
-    setEditableTaskId(null); // After editing, set the task to non-editable
-  };
-
-  const toggleTask = (taskId) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const handleDragEnd = (result) => {
-    if (!result.destination) return; // Dropped outside the list
-    const reorderedTasks = Array.from(tasks);
-    const [movedTask] = reorderedTasks.splice(result.source.index, 1);
-    reorderedTasks.splice(result.destination.index, 0, movedTask);
-    setTasks(reorderedTasks);
-  };
-
   return (
-    <div className="App">
-      <h1>To-Do List</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Add a new task"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <button onClick={addTask}>
-          <BsPlus /> Add
-        </button>
-      </div>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="tasks">
-          {(provided) => (
-            <ul {...provided.droppableProps} ref={provided.innerRef}>
-              {tasks.map((task, index) => (
-                <Draggable key={task.id} draggableId={task.id} index={index}>
-                  {(provided) => (
-                    <li
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <input
-                        type="checkbox"
-                        checked={task.completed}
-                        onChange={() => toggleTask(task.id)}
-                      />
-                      {editableTaskId === task.id ? (
-                        <>
-                          <input
-                            type="text"
-                            value={task.text}
-                            onChange={(e) => editTask(task.id, e.target.value)}
-                          />
-                          <button onClick={() => setEditableTaskId(null)}>
-                            <BsCheck /> Save
-                          </button>
-                        </>
-                      ) : (
-                        <>
-                          <span>{task.text}</span>
-                          <button onClick={() => setEditableTaskId(task.id)}>
-                            <BsPencil /> Edit
-                          </button>
-                        </>
-                      )}
-                      <button onClick={() => removeTask(task.id)}>
-                        <BsFillTrashFill /> Remove
-                      </button>
-                    </li>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={[usersList]} />
     </div>
   );
-};
+}
 
 export default App;
